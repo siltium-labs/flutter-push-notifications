ab4d90f15fd3b0469f266bac07682a9c0e16fa29
YamiTeyssier <97235275+YamiTeyssier@users.noreply.github.com>
2023-09-27 11:26:45 -0300

    add fvm & migrate plugin to 3.13.5

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\yamil\Documents\Siltium\SPN-PLUGIN\s_push_notifications\example"
+export "FLUTTER_APPLICATION_PATH=C:\Users\yamil\Documents\Siltium\SPN_PLUGIN\flutter-push-notifications\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
